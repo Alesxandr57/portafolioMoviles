bc53f2e83409126219525a462b813645a838b466
Eudaldo <eaav0404@gmail.com>
2026-02-13 12:00:13 -0700

    Se hizo la modificacion del codigo 13_menu.dart usando la estructura de control switch

--- 13_menu.dart
+++ 13_menu.dart
@@ -1,60 +1,91 @@
 import 'dart:io';
 import 'dart:math';
 
-main() {
-  int i = 1;
-  while (i > 0) {
-    print("Desea calcular un Area o Volumen? \n Area = '1' - Volumen = '2'\n");
-    int? Eleccion = int.parse(stdin.readLineSync()!);
-    if (Eleccion == 1) {
-      print(
-        "Desea calcular el Area de un Circulo, Triangulo o Rectangulo? \n Circulo = '1' - Triangulo = '2' - Rectangulo = '3'\n",
-      );
-      int? Areas = int.parse(stdin.readLineSync()!);
-      if (Areas == 1) {
-        print("Ingresa el radio del circulo");
-        double? radio = double.parse(stdin.readLineSync()!);
-        double Area1 = (3.1416 * (pow(radio, 2)));
-        print("El Area del Circulo es de: $Area1 m²\n");
-      } else if (Areas == 2) {
-        print("Ingresa el valor de la base del triangulo");
-        double? base1 = double.parse(stdin.readLineSync()!);
-        print("Ingresa el valor de la altura del triangulo");
-        double? altura1 = double.parse(stdin.readLineSync()!);
-        double Area2 = (base1 * altura1) / 2;
-        print("El Area del Triangulo es de: $Area2 m²\n");
-      } else {
-        print("Ingresa el valor de la base del Rectangulo");
-        double? base2 = double.parse(stdin.readLineSync()!);
-        print("Ingresa el valor de la altura del Rectangulo");
-        double? altura2 = double.parse(stdin.readLineSync()!);
-        double Area3 = (base2 * altura2);
-        print("El Area del Rectangulo es de: $Area3 m²\n");
-      }
+void main() {
+  while (true) {
+    print("¿Qué deseas calcular?");
+    print("1 = Área");
+    print("2 = Volumen");
+
+    int opcion = int.parse(stdin.readLineSync()!);
+
+    if (opcion == 1) {
+      calcularArea();
+    } else if (opcion == 2) {
+      calcularVolumen();
     } else {
-      print(
-        "Desea calcular el Volumen de un Esfera, Piramide o Cubo? \n Esfera = '1' - Piramide = '2' - Cubo = '3'\n",
-      );
-      int? Volumenes = int.parse(stdin.readLineSync()!);
-      if (Volumenes == 1) {
-        print("Ingresa el Radio de la Esfera");
-        double? radioEsfera = double.parse(stdin.readLineSync()!);
-        double Volumen1 = (4 / 3) * pi * pow(radioEsfera, 3);
-        print("El Volumen de la Esfera es de: $Volumen1 m³\n");
-      } else if (Volumenes == 2) {
-        print("Ingresa el valor de la base de la Piramide");
-        double? basePiramide = double.parse(stdin.readLineSync()!);
-        print("Ingresa el valor de la altura de la Piramide");
-        double? alturaPiramide = double.parse(stdin.readLineSync()!);
-        double Volumen2 = (1 / 3) * basePiramide * alturaPiramide;
-        print("El Volumen de la Piramide es de: $Volumen2 m³\n");
-      } else {
-        print("Ingresa la Longitud de la arista del Cubo");
-        double? arista = double.parse(stdin.readLineSync()!);
-        double Volumen3 = pow(arista, 3).toDouble();
-        print("El Volumen del Cubo es de: $Volumen3 m³\n");
-      }
+      print("Opción no válida\n");
     }
-    i = i + 1;
   }
 }
+
+void calcularArea() {
+  print("\nÁrea de:");
+  print("1 = Círculo");
+  print("2 = Triángulo");
+  print("3 = Rectángulo");
+
+  int figura = int.parse(stdin.readLineSync()!);
+
+  switch (figura) {
+    case 1:
+      print("Ingresa el radio:");
+      double r = double.parse(stdin.readLineSync()!);
+      print("Área del Círculo: ${pi * pow(r, 2)} m²\n");
+      break;
+
+    case 2:
+      print("Ingresa la base:");
+      double b = double.parse(stdin.readLineSync()!);
+      print("Ingresa la altura:");
+      double h = double.parse(stdin.readLineSync()!);
+      print("Área del Triángulo: ${(b * h) / 2} m²\n");
+      break;
+
+    case 3:
+      print("Ingresa la base:");
+      double b = double.parse(stdin.readLineSync()!);
+      print("Ingresa la altura:");
+      double h = double.parse(stdin.readLineSync()!);
+      print("Área del Rectángulo: ${b * h} m²\n");
+      break;
+
+    default:
+      print("Opción no válida\n");
+  }
+}
+
+void calcularVolumen() {
+  print("\nVolumen de:");
+  print("1 = Esfera");
+  print("2 = Pirámide");
+  print("3 = Cubo");
+
+  int figura = int.parse(stdin.readLineSync()!);
+
+  switch (figura) {
+    case 1:
+      print("Ingresa el radio:");
+      double r = double.parse(stdin.readLineSync()!);
+      print("Volumen de la Esfera: ${(4 / 3) * pi * pow(r, 3)} m³\n");
+      break;
+
+    case 2:
+      print("Ingresa el área de la base:");
+      double base = double.parse(stdin.readLineSync()!);
+      print("Ingresa la altura:");
+      double h = double.parse(stdin.readLineSync()!);
+      print("Volumen de la Pirámide: ${(1 / 3) * base * h} m³\n");
+      break;
+
+    case 3:
+      print("Ingresa la arista:");
+      double a = double.parse(stdin.readLineSync()!);
+      print("Volumen del Cubo: ${pow(a, 3)} m³\n");
+      break;
+
+    default:
+      print("Opción no válida\n");
+  }
+}
+
